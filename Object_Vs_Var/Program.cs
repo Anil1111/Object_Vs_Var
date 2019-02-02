32af1ce6537cb2c27d2eeba9b93282ae26662d92
TheAnilDeveloper <anilsri.04@gmail.com>
2019-02-02 22:58:01 +0530

    Update Program.cs

--- Object_Vs_Var/Program.cs
+++ Object_Vs_Var/Program.cs
@@ -10,6 +10,15 @@ namespace Object_Vs_Var
     {
         static void Main(string[] args)
         {
+            List<object> myList = new List<object>();
+            myList.Add(1);
+            myList.Add("We are all greatful to Sir Dennis Ritchie");
+            myList.Add(true);
+            myList.Add(120.40);
+
+            foreach (var v in myList)
+            Console.WriteLine(v);
+            Console.ReadLine();
         }
     }
 }
